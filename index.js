4cbc6c68e2dddb72a9b9c232ba155875e39a2ff9
thijs van slooten <41576972+DSBxASSASSIN@users.noreply.github.com>
2020-02-09 19:05:37 +0100

    idk

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const Discord = require("discord.js");
 require('dotenv').config();
 const bot = new Discord.Client();
+const ytdl = require("ytdl-core");
 
 const token = process.env.TOKEN_KEY;
 
 const PREFIX = "?";
 const AUTHOR = "DSBxASSASSIN";
 const VERSION = "1.0.0";
-const BOTNAME = "DSB_bot"
+const BOTNAME = "DSB_bot";
+var servers = {};
 
 bot.on("ready", ()=>{
 	console.log(`logged in as ${bot.user.tag}`);
@@ -15,17 +17,26 @@ bot.on("ready", ()=>{
 
 
 bot.on('message', message=>{
-	let args = message.content.substring(PREFIX.length).split(" ")
+	let args = message.content.substring(PREFIX.length).split(" ");
+	let message_list = message.content.split(" ");
+	
+	for(let i = 0; i < message_list.length; i++){
+		if(message_list[i] === "furry" || message_list[i] === "furries"){
+			message.reply("Fucking FURRY");
+		}
+	}
+
+
 	
 	switch(args[0]){
 		case "info":
 			switch(args[1]){
 				case "author":
 					message.channel.send(`the author is ${AUTHOR}`);
-					break;
+				break;
 				case "version":
 					message.channel.send(`the version is ${VERSION}`);
-					break;
+				break;
 				default:
 					const embed = new Discord.RichEmbed()
 					.setTitle("bot info")
@@ -35,16 +46,79 @@ bot.on('message', message=>{
 					.addField("Current Server", message.guild.name)
 					.setColor("9B0000")
 					message.channel.send(embed);
-					break;
+				break;
 			}
-			break;
+		break;
+
 		case "clear":
 			if(!args[1]){
 				mesasge.channel.send("Invallid Argument");
 			}else{
 				message.channel.bulkDelete(args[1]);
 			}
-			break;
+		break;
+
+		case "play":
+			function  play(connection, message){
+				var server = servers[message.guild.id];
+
+				server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
+
+				server.queue.shift();
+
+				server.dispatcher.on("end", function(){
+					if(server.queue[0]){
+						play(connection, message);
+					}else{
+						connection.disconnect();
+					}
+				});
+
+			}
+
+			if(!args[1]){
+				message.reply("Invallid Argument");
+				return;
+			}
+			
+			if(!message.member.voiceChannel){
+				message.reply("you must be in an channel to play music");
+				return;
+			}
+
+			if(!servers[message.guild.id]) servers[message.guild.id] = {
+				queue: []
+			}
+
+			var server = servers[message.guild.id];
+
+			server.queue.push(args[1]);
+
+			if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection){
+				play(connection, message);
+			})
+			
+		break;
+
+		case "skip":
+			var server = servers[message.guild.id];
+			if(server.dispatcher) server.dispatcher.end();
+			message.channel.send("skipped song")
+		break;
+			
+		case "stop":
+			var server = servers[message.guild.id];
+			if(message.guild.voiceConnection){
+				for(var i = server.queue.length -1; i >= 0; i--){
+					server.queue.splice(i, 1);
+				}
+
+				server.dispatcher.end();
+			}
+
+			if(message.guild.connection) message.guild.voiceConnection.disconnect();
+		break;
+			
 	}
 })
 
